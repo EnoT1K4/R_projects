b8f385485db539e179408bacbead51bd9e57d12b
killed-by-kitty <dubrovskij_1999@inbox.ru>
2023-02-14 19:08:55 +0300

    new R file

---
+++ neww.R
@@ -0,0 +1,27 @@
+print("smth")
+cat("cat")
+
+var_num_1 <-  56
+
+
+
+15+6*sin(32)+exp(1i*6)
+
+
+is.logical(NaN)
+
+is.numeric(12)
+
+is.character('efl;')
+
+var_vec_bool <- logical(15)
+var_vec_bool
+var_vec_numeric <- numeric(10)
+var_vec_numeric
+var_vec_character <- character(8)
+var_vec_character
+vec_combine <- c(var_vec_numeric, c(1,4,5),8)
+vec_combine
+
+set.seed(123)
+
